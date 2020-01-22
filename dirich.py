617fe620cdc53cbde3fbb0b9d92de7358a48e171
mohammadi.hossein <mohammadi.hossein@std.izu.edu.tr>
2020-01-22 12:10:59 +0300

    dirich.py

---
+++ dirich.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+def dirich(omega,L):
+
+    """
+    DIRICH   compute sin(L*omega/2)/Lsin(omega/2)   
+    ------
+    Usage :   D = dirich(omega, L)
+
+    omega : argument of Dirichlet function   (works for matrix omega)
+    L     : length of corresponding FIR filter
+
+    """
+
+    denom = np.sin(0.5*np.asarray(omega))
+    zdenom = abs(denom)<1e-10
+    D = zdenom + np.logical_not(zdenom)*np.sin((L/2)*np.array(omega))/(L*denom + zdenom) 
+    return D
+
+
+
+
+
+
+
+
+
+
