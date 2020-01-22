18cf6deff4f657f2dc9156033b2db6380bcec0a9
mohammadi.hossein <mohammadi.hossein@std.izu.edu.tr>
2020-01-22 12:11:36 +0300

    firfilt.py

---
+++ firfilt.py
@@ -0,0 +1,30 @@
+#!/bin/usr/env python 
+
+import numpy as np
+from scipy.signal import upfirdn
+
+
+
+def firfilt (bb, xx):
+    '''FIRFILT   FIR filter implemented as a difference equation
+
+    usage:   yy = firfilt(bb, xx) 
+
+    implements the FIR filter difference equation:
+
+                   M-1
+                   __
+                   \
+           y[n]=   /  b[k] * x[n-k]
+                   --
+                   k=0
+
+     The length of the resulting vector is  length(bb)+length(xx)-1.
+
+    '''
+    bb = np.asarray(bb)
+    xx = np.asarray(xx)
+
+    y = upfirdn(bb, xx)
+
+    return y
