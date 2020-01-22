a23db07046d2168188e82f1c994b2d170520b2c5
mohammadi.hossein <mohammadi.hossein@std.izu.edu.tr>
2020-01-22 12:11:12 +0300

    dtft.py

---
+++ dtft.py
@@ -0,0 +1,28 @@
+#!/bin/usr/env python 
+
+import numpy as np
+from  math import pi, ceil, floor
+
+
+def dtft (h, N):
+  '''DTFT   calculate DTFT at N equally spaced frequencies
+  Usage:   [H, W] = dtft(h, N)
+    h : finite-length input vector, whose length is L
+    N : number of frequencies for evaluation over [-pi,pi)
+            ==> constraint: N >= L 
+    H : DTFT values (complex)
+    W : (2nd output) vector of freqs where DTFT is computed
+
+  '''
+
+  h = np.asarray(h)
+  N = int(np.fix(N))
+  L = len(h)
+  if (N < L):
+      print('DTFT: # data samples cannot exceed # freq samples')
+  W = (2 * pi/N) * np.arange(0, N, 1)
+  mid = int(ceil(N/2) + 1)
+  W[mid-1:N] =  W[mid-1:N] -2*pi
+  W = np.fft.fftshift(W)
+  H = np.fft.fftshift(np.fft.fft(h,N))
+  return H, W
